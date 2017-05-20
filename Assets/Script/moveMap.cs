69c779152bc73a807c910e9e7d51142d5fdaa61c
syamamoto-pastoraldog <shimpei@pd-sy.local>
2017-05-20 14:35:44 +0900

    select map

---
+++ Assets/Script/moveMap.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; 
+
+public class moveMap : MonoBehaviour {	
+	public void MainMap () {
+		SceneManager.LoadScene("main_map");
+	}
+
+	public void LoadHouse () {
+		SceneManager.LoadScene("house");
+	}
+
+	public void LoadDakko () {
+		SceneManager.LoadScene("dakko");
+	}
+
+	public void LoadTakeOut () {
+		SceneManager.LoadScene("take out");
+	}
+		
+	public void LoadPark () {
+		SceneManager.LoadScene("park");
+	}
+
+}
