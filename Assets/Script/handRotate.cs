f0a5259407e005a05bbdecd306e6d41e462df7aa
syamamoto-pastoraldog <shimpei@pd-sy.local>
2017-06-24 17:40:57 +0900

    add nadenade

---
+++ Assets/Script/handRotate.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class handRotate : MonoBehaviour {	
+	void Update () {
+		float sensitivity = 10f; // いわゆるマウス感度
+		float mouse_move_x = Input.GetAxis("Mouse X") * sensitivity;
+		float mouse_move_y = Input.GetAxis("Mouse Y") * sensitivity;
+
+		transform.Rotate(new Vector3(mouse_move_y, -1 * mouse_move_x, 0));
+	}
+}
